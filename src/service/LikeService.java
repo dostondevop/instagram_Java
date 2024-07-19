09142816e068bd1a6f111922ddcedc72dbce6226
azizaxon13 <erkinbaevaaziza.13.com@gmail.com>
2024-07-19 14:08:16 +0500

    Like service added

--- src/service/LikeService.java
+++ src/service/LikeService.java
@@ -16,6 +16,49 @@ public class LikeService implements BaseService<Like> {
 
     @Override
     public Like add(Like like) {
-        return null;
+        List<Like> likes = io.read(PATH);
+        boolean wasLiked = wasLiked(like.getPostId(), like.getUserId());
+        if (!wasLiked) {
+            likes.add(like);
+            io.write(PATH, likes);
+            return like;
+        } else {
+            likes.remove(like);
+            io.write(PATH, likes);
+            return null;
+        }
+    }
+
+    public boolean dislike(UUID postId, UUID userId) {
+        List<Like> likes = io.read(PATH);
+        for (Like like : likes) {
+            if (like.getPostId().equals(postId) && like.getUserId().equals(userId)) {
+                likes.remove(like);
+                io.write(PATH, likes);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean wasLiked(UUID postId, UUID userId) {
+        List<Like> likes = io.read(PATH);
+        for (Like l : likes) {
+            if (l.getPostId().equals(postId) && l.getUserId().equals(userId)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<Like> getLikesByPostId(UUID postId) {
+        List<Like> likes = io.read(PATH);
+        List<Like> likesByPostId = new ArrayList<>();
+        for (Like like : likes) {
+            if (like.getPostId().equals(postId)) {
+                likesByPostId.add(like);
+            }
+        }
+        return likesByPostId;
     }
 }
