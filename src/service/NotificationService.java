66ddda4671d4e7bf449b1ce029d950a37ff94ac4
simpleeky <davronmahmarajabov370@gmail.com>
2024-07-19 14:11:04 +0500

    added notificationservice and userservice

--- src/service/NotificationService.java
+++ src/service/NotificationService.java
@@ -1,10 +1,68 @@
 package service;
 
+import com.google.gson.reflect.TypeToken;
+import models.Comment;
 import models.Notification;
+import models.Post;
+import models.User;
+
+import java.lang.reflect.Type;
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class NotificationService implements BaseService<Notification> {
+    public static final String PATH = "C:\\E\\Wanna_Learn\\Java\\instagram\\src\\Docs\\notifications.json";
+    Type listType = new TypeToken<List<Notification>>() {}.getType();
+    IOUtilsService<Notification> ioService = new IOUtilsService<>(listType);
+    PostService postService;
+    UserService userService;
+
+    public NotificationService(PostService postService, UserService userService) {
+        this.postService = postService;
+        this.userService = userService;
+    }
+
     @Override
     public Notification add(Notification notification) {
+        List<Notification> notifications = ioService.read(PATH);
+        addIsRead(notification);
+        notifications.add(notification);
+        ioService.write(PATH, notifications);
+        return notification;
+    }
+
+    private void addIsRead(Notification notification) {
+        List<User> users = userService.getUsers();
+        for (User user : users) {
+            notification.getIsReadMap().put(user.getId(), false);
+        }
+    }
+
+    public Notification updateNotification(Notification notification) {
+        List<Notification> notifications = ioService.read(PATH);
+        for (Notification notification1 : notifications) {
+            if (notification.getId().equals(notification1.getId())) {
+                notification1.setIsReadMap(notification.getIsReadMap());
+                ioService.write(PATH, notifications);
+                return notification1;
+            }
+        }
         return null;
+    }
+
+    public List<Notification> getNewNotifications(UUID userId) {
+        List<Notification> notifications = ioService.read(PATH);
+        List<Notification> filteredNotifications = new ArrayList<>();
+        for (Notification notification : notifications) {
+            if (!notification.getUserId().equals(userId)) {
+                if (!notification.getIsReadMap().get(userId)) {
+                    filteredNotifications.add(notification);
+                }
+            }
+
+        }
+        return filteredNotifications;
     }
 }
--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,10 +1,69 @@
 package service;
 
+import com.google.gson.reflect.TypeToken;
+import models.Comment;
+import models.Like;
 import models.User;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserService implements BaseService<User> {
+    public static final String PATH = "C:\\E\\Wanna_Learn\\Java\\instagram\\src\\Docs\\users.json";
+    Type listType = new TypeToken<List<User>>() {}.getType();
+    IOUtilsService<User> ioUtilsService = new IOUtilsService<>(listType);
+    LikeService likeService;
+
+    public UserService(LikeService likeService) {
+        this.likeService = likeService;
+    }
+
     @Override
     public User add(User user) {
+        List<User> users = ioUtilsService.read(PATH);
+        if (hasUser(user)) {
+            return null;
+        }
+        users.add(user);
+        ioUtilsService.write(PATH, users);
+        return user;
+    }
+
+    public User login(String username, String password) {
+        List<User> users = ioUtilsService.read(PATH);
+        for (User user : users) {
+            if (user.getUserName().equals(username) && user.getPassword().equals(password)) {
+                return user;
+            }
+        }
         return null;
+    }
+
+    private boolean hasUser(User user) {
+        List<User> users = ioUtilsService.read(PATH);
+        for (User u : users) {
+            if (u.getUserName().equals(user.getUserName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<User> getLikedUsers(List<Like> likes) {
+        List<User> users = ioUtilsService.read(PATH);
+        List<User> likedUsers = new ArrayList<>();
+        for (Like like : likes) {
+            for (User user : users) {
+                if (like.getUserId().equals(user.getId())) {
+                    likedUsers.add(user);
+                }
+            }
+        }
+        return likedUsers;
+    }
+
+    public List<User> getUsers() {
+        return ioUtilsService.read(PATH);
     }
 }
